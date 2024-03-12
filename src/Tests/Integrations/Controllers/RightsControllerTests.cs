571dd1ce061be50291380da43ed0007d7016a126
Mikhail Bereberdin <bereberdin999@icloud.com>
2024-03-12 11:12:19 +0700

    #10 Добавление интеграционных тестов для прав
    - Добавлен класс интеграционнных тестов для прав.

---
+++ src/Tests/Integrations/Controllers/RightsControllerTests.cs
@@ -0,0 +1,88 @@
+namespace Tests.Integrations.Controllers;
+
+using System.Net;
+
+using Domain.DTOs.Games;
+using Domain.DTOs.Rights;
+
+using FluentAssertions;
+
+using Tests.Extensions;
+using Tests.Fixtures;
+
+using WebApi.Controllers;
+
+/// <summary>
+/// Интеграционные тесты для <see cref="RightsController"/>.
+/// </summary>
+public class RightsControllerTests : IntegrationFixture
+{
+    /// <inheritdoc cref="RightsControllerTests"/>
+    /// <param name="factory">Фабрика проекта WebApi.</param>
+    public RightsControllerTests(WebApiApplicationFactory<Program> factory) : base(factory)
+    {
+    }
+
+
+    #region Add
+
+    [Fact]
+    public async Task RegisterRightsAsync_WithCorrectRightsRegistrationDto_ReturnOk()
+    {
+        //Arrange
+        var game = new ExternalGameDto("TestGame");
+        var rights = new List<ExternalCreateRightDto>
+        {
+            new("TestRight", "Тестовое описание права."),
+            new("TestRight1", "Тестовое описание права1."),
+            new("TestRight2", "Тестовое описание права2.")
+        };
+        var rightsToRegisterDto = new RegisterRightsDto(rights, game);
+
+        //Act
+        var response = await Client.PostAsync("rights/register", rightsToRegisterDto.ToStringContent());
+
+        //Assert
+        response.StatusCode.Should().Be(HttpStatusCode.OK);
+    }
+
+    [Fact]
+    public async Task RegisterRightsAsync_WithIncorrectRights_ReturnBadRequest()
+    {
+        //Arrange
+        var game = new ExternalGameDto("TestGame");
+        var rights = new List<ExternalCreateRightDto>
+        {
+            new(null!, null!),
+            new("TestRight1", null!),
+            new(null!, "Тестовое описание права2.")
+        };
+        var rightsToRegisterDto = new RegisterRightsDto(rights, game);
+
+        //Act
+        var response = await Client.PostAsync("rights/register", rightsToRegisterDto.ToStringContent());
+
+        //Assert
+        response.StatusCode.Should().Be(HttpStatusCode.BadRequest);
+    }
+
+    [Fact]
+    public async Task RegisterRightsAsync_WithIncorrectGame_ReturnBadRequest()
+    {
+        //Arrange
+        var game = new ExternalGameDto(null!);
+        var rights = new List<ExternalCreateRightDto>
+        {
+            new("TestRight", "Тестовое описание права.")
+        };
+        var rightsToRegisterDto = new RegisterRightsDto(rights, game);
+
+        //Act
+        var response = await Client.PostAsync("rights/register", rightsToRegisterDto.ToStringContent());
+
+        //Assert
+        response.StatusCode.Should().Be(HttpStatusCode.BadRequest);
+    }
+
+    #endregion
+}
